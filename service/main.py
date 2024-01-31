f3536ae7c02f6db79452b4c7af99287c1a51fdf1
Neidn <dty3152@gmail.com>
2024-01-31 14:09:40 +0900

    make first snake segments

--- service/main.py
+++ service/main.py
@@ -5,9 +5,19 @@ size = {
     "height": 600,
 }
 
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+segement_list = []
+
 screen = Screen()
 screen.setup(width=size["width"], height=size["height"])
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+for position in starting_position:
+    segement = Turtle("square")
+    segement.color("white")
+    segement.goto(position)
+    segement_list.append(segement)
 
 screen.exitonclick()
